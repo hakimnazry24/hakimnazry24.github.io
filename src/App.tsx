cb13dc5adc1b8e0b80fdca2dccfafe5f3d82a72a
hakimnazry <hakimnazry@gmail.com>
2024-05-15 02:25:30 +0800

    mobile friendly

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { motion } from "framer-motion";
 import Card from "./components/card";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
+import { TfiEmail } from "react-icons/tfi";
 
 export default function App() {
   return (
@@ -12,7 +15,7 @@ export default function App() {
         </a>
         <div>
           <ul className="flex gap-5">
-          <li className="transition ease-in-out duration-150 p-3 hover:bg-white hover:text-black rounded-2xl">
+            <li className="transition ease-in-out duration-150 p-3 hover:bg-white hover:text-black rounded-2xl">
               <a href="#projects">Projects</a>
             </li>
             <li className="transition ease-in-out duration-150 p-3 hover:bg-white hover:text-black rounded-2xl">
@@ -28,25 +31,25 @@ export default function App() {
         className="flex justify-center items-center mb-16"
         style={{ height: "80vh" }}
       >
-        <div className="grid grid-cols-2 gap-3 items-center ">
+        <div className="md:lg:grid grid-cols-2 gap-3 items-center ">
           <div className="justify-self-center">
             <img
               src="/patapata.gif"
               alt=""
               style={{ height: "20rem" }}
-              className="rounded-2xl"
+              className="rounded-2xl hidden md:lg:block"
             />
           </div>
           <div>
-            <h1 className="text-5xl font-semibold ">Hello</h1>
-            <h2 className="text-2xl mb-10">
+            <h1 className="text-5xl font-semibold text-center md:lg:text-left">Hello</h1>
+            <h2 className="text-2xl mb-10 text-center md:lg:text-left">
               My name is{" "}
               <span className="bg-gradient-to-r from-green-500 via-violet-600 to-blue-400 bg-clip-text text-transparent inline-block font-semibold">
                 Hakim
               </span>
             </h2>
-            <h3 className="mb-2">I have experience using</h3>
-            <ul className="flex gap-3 flex-wrap">
+            <h3 className="mx-5 md:lg:mx-0 mb-2">I have experience using</h3>
+            <ul className="flex gap-3 flex-wrap mx-5 md:lg:mx-0">
               <li className="p-3 rounded-2xl text-white bg-gradient-to-r from-green-500 via-violet-600 to-blue-400 font-semibold transition ease-in-out hover:-translate-y-2 hover:translate-x-1">
                 HTML
               </li>
@@ -98,10 +101,24 @@ export default function App() {
       </div>
 
       {/* about */}
-      <div className="flex justify-center items-center mb-10 pt-20" id="about">
+      <div className="flex justify-center items-center my-10 pt-20" id="about">
         <div className="mx-16 text-center " style={{ height: "100vh" }}>
-          <motion.h1 className="text-5xl font-semibold mb-5" initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 0.5}} viewport={{once: true}}>About</motion.h1>
-          <motion.p className=" md:lg:mx-32" initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 0.5}} viewport={{once: true}}>
+          <motion.h1
+            className="text-5xl font-semibold mb-5"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+          >
+            About
+          </motion.h1>
+          <motion.p
+            className=" md:lg:mx-32"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+          >
             Hey there! I'm Muhammad Hakim, hailing from the beautiful city of
             Melaka. Currently, you can find me diving into the world of computer
             science at the International Islamic University Malaysia. When I'm
@@ -111,38 +128,115 @@ export default function App() {
             even the Malaysian tradisional music instrument, the Gamelan. Reach
             me out and let's connect :D
           </motion.p>
-          <div className="flex justify-center gap-20 mt-10">
-            <motion.img initial={{opacity: 0}} whileInView={{opacity:1}} transition={{delay: 0.5}} viewport={{once: true}}  src="/image2.jpeg" alt="" style={{width: "15rem"}} className="rounded-2xl self-start" />
-            <motion.img initial={{opacity: 0}} whileInView={{opacity:1}} transition={{delay: 0.5}} viewport={{once: true}}  src="/image1.JPG" alt="" style={{width: "15rem"}} className="rounded-2xl self-start" />
-            <motion.img initial={{opacity: 0}} whileInView={{opacity:1}} transition={{delay: 0.5}} viewport={{once: true}}  src="/image3.jpeg" alt="" style={{width: "15rem"}} className="rounded-2xl self-start" />
+          <div className="flex flex-col md:lg:flex-row justify-center gap-20 mt-10" >
+            <motion.img
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ delay: 0.5, duration: 1 }}
+              viewport={{ once: true }}
+              src="/image2.jpeg"
+              alt=""
+              style={{ width: "15rem" }}
+              className="rounded-2xl self-start mb-5 md:lg:mb-0 hidden md:lg:block"
+            />
+            <motion.img
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ delay: 0.5, duration: 1 }}
+              viewport={{ once: true }}
+              src="/image1.JPG"
+              alt=""
+              style={{ width: "15rem" }}
+              className="rounded-2xl self-start mb-5 md:lg:mb-0 hidden md:lg:block" 
+            />
+            <motion.img
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ delay: 0.5, duration: 1 }}
+              viewport={{ once: true }}
+              src="/image3.jpeg"
+              alt=""
+              style={{ width: "15rem" }}
+              className="rounded-2xl self-start mb-5 md:lg:mb-0 hidden md:lg:block"
+            />
           </div>
         </div>
       </div>
 
       {/* projects */}
-      <div className="flex justify-center h-screen pt-10" id="projects">
+      <div className="flex justify-center mb:lg:h-screen pt-10" id="projects">
         <div>
-          <motion.h1 className="text-center text-5xl font-semibold mb-5" initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 0.5}} viewport={{once: true}}>Projects</motion.h1>
-          <div className="md:lg: grid grid-cols-3 gap-10">
-          <Card link="/project1.png" title="Motion-U Technopreneurship Club Official Website" desc="Motion-U Club is a technopreneurship club based in International Islamic University Malaysia. I created the website using NextJs, Tailwind and Contentful Headless CMS."></Card>
-          <Card link="/project2.png" title="KICT Online Food Order" desc="KICT Online Food Order is an food ordering system that is meant for community at Kulliyyah of Information and Communication Technology (KICT) IIUM. This project is built using React, NodeJS, SQLite, and Tailwind"></Card>
-          <Card link="/project3.png" title="Motion-U Program Management System" desc="Motion-U Program Management System is a system that helps Motion-U mainboards to manage club's program. It is built using React, NodeJS, SQLite and Tailwind"></Card>
+          <motion.h1
+            className="text-center text-5xl font-semibold mb-5"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+          >
+            Projects
+          </motion.h1>
+          <div className="md:lg:grid grid-cols-3 gap-10">
+            <Card
+              link="/project1.png"
+              title="Motion-U Technopreneurship Club Official Website"
+              desc="Motion-U Club is a technopreneurship club based in International Islamic University Malaysia. I created the website using NextJs, Tailwind and Contentful Headless CMS."
+            ></Card>
+            <Card
+              link="/project2.png"
+              title="KICT Online Food Order"
+              desc="KICT Online Food Order is an food ordering system that is meant for community at Kulliyyah of Information and Communication Technology (KICT) IIUM. This project is built using React, NodeJS, SQLite, and Tailwind"
+            ></Card>
+            <Card
+              link="/project3.png"
+              title="Motion-U Program Management System"
+              desc="Motion-U Program Management System is a system that helps Motion-U mainboards to manage club's program. It is built using React, NodeJS, SQLite and Tailwind"
+            ></Card>
+          </div>
+        </div>
+      </div>
+
+      {/* contact */}
+      <div className="flex justify-center py-10 text-center" id="contact">
+        <div>
+          <h1 className="text-5xl font-semibold mb-20">Contact</h1>
+          <div className="md:lg:grid grid-cols-4 gap-5">
+            <div className="flex flex-col items-center gap-2 mb-5 md:lg:mb-0">
+              <TfiEmail style={{ fontSize: "5rem" }} />
+              <p>
+                hakimnazry@gmail.com
+              </p>
+            </div>
+            <div className="flex flex-col items-center gap-2 mb-5 md:lg:mb-0">
+              <FaGithub style={{ fontSize: "5rem" }} />
+              <p>
+                <a href="www.github.com/hakimnazry24">
+                  github.com/hakimnazry24
+                </a>
+              </p>
+            </div>
+            <div className="flex flex-col items-center gap-2 mb-5 md:lg:mb-0">
+              <BsTwitterX style={{ fontSize: "5rem" }} />
+              <p>@hakimnazry__</p>
+            </div>
+            <div className="flex flex-col items-center gap-2 mb-5 md:lg:mb-0">
+              <FaLinkedin style={{ fontSize: "5rem" }} />
+              <p>www.linkedin.com/in/hakim-nazri/</p>
+            </div>
           </div>
         </div>
       </div>
 
       {/* footer */}
-      
-        <footer className="w-full mb-10 flex justify-center md:lg:mt-40 absolute py-5">
-          <p className="text-slate-600">
-            Made with{" "}
-            <span className="bg-gradient-to-r from-green-500 via-violet-600 to-blue-400 bg-clip-text text-transparent inline-block font-semibold">
-              ♥
-            </span>{" "}
-            by Hakim
-          </p>
-        </footer>
-      
+
+      <footer className="w-full mb-10 flex justify-center md:lg:mt-40 absolute py-5">
+        <p className="text-slate-600">
+          Made with{" "}
+          <span className="bg-gradient-to-r from-green-500 via-violet-600 to-blue-400 bg-clip-text text-transparent inline-block font-semibold">
+            ♥
+          </span>{" "}
+          by Hakim
+        </p>
+      </footer>
     </>
   );
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -9,7 +9,7 @@ type CardProp = {
 export default function Card({ link, title, desc }: CardProp) {
   return (
     <>
-      <motion.div className="max-w-sm rounded overflow-hidden shadow-lg" whileHover={{scale: 1.1}} initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 0.2}} viewport={{once: true}}>
+      <motion.div className="max-w-sm rounded overflow-hidden shadow-lg mb-7 md:lg:mb-0" whileHover={{scale: 1.1}} initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 0.2}} viewport={{once: true}}>
         <img className="w-full" src={link} />
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{title}</div>
