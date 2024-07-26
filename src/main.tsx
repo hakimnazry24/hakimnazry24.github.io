fafcfc00a04925526807eb6316201248567a3e1a
hakimnazry <hakimnazry@gmail.com>
2024-07-26 10:15:05 +0800

    remove react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LandingPageIIUMFreelancer from "./iium_freelancer/landing_page.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App></App>,
-  },
-  {
-    path: "iium-freelancer",
-    element: <LandingPageIIUMFreelancer></LandingPageIIUMFreelancer>,
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <App />
   </React.StrictMode>
 );
