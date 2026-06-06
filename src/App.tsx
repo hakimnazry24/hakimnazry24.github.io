2f8e69be0a911a294ffc3b54cdcd0b32725ca10c
hakimnazry24 <hakimnazry@gmail.com>
2026-06-07 03:14:07 +0800

    remove unused imports for Experience and Projects components

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { Helmet } from "react-helmet";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
 import CV from "./components/CV";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
